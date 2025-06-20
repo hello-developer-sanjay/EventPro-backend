bd9d4db262ccab8074c566ce13ba63b87c8bb1e9
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 16:33:26 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,73 +1,43 @@
-// config/passport.js
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { jwtSecret } = require('../config/auth');
-const { sendWelcomeEmail } = require('./mailer');
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://eventmanager-api-19july.onrender.com/api/auth/google/callback",
-    passReqToCallback: true
-  },
-  async (req, token, tokenSecret, profile, done) => {
-    console.log('GoogleStrategy callback executed');
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-      console.log('User lookup:', user);
-
-      if (!user) {
-        user = new User({
-          googleId: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          role: 'user'
-        });
-        await user.save();
-        console.log('New user created:', user);
-      }
-
-      // Generate JWT token
-      const payload = {
-        user: {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          role: user.role
-        }
-      };
-
-      const token = jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
-      user.token = token;
-
-      console.log('Generated JWT token:', token);
-
-      // Send welcome email
-      sendWelcomeEmail(user, req);
-
-      return done(null, user);
-    } catch (err) {
-      console.error('Error in GoogleStrategy:', err);
-      return done(err, false);
-    }
-  }
-));
 
 passport.serializeUser((user, done) => {
-  console.log('Serializing user:', user);
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  console.log('Deserializing user with id:', id);
   try {
     const user = await User.findById(id);
     done(null, user);
-  } catch (err) {
-    done(err, null);
+  } catch (error) {
+    done(error, null);
   }
 });
 
-module.exports = passport;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'https://eventmanager-api-19july.onrender.com/api/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          return done(null, user);
+        }
+        user = new User({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+        });
+        await user.save();
+        done(null, user);
+      } catch (error) {
+        done(error, null);
+      }
+    }
+  )
+);
