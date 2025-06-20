0913fb0b8f30cf5f9131324996239339f6961cd2
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 14:00:06 +0530

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -1,42 +1,48 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-    eventName: {
-        type: String,
-        required: true
-    },
-    eventType: {
-        type: String,
-        required: true
-    },
-    startDate: {
-        type: Date,
-        required: true
-    },
-    endDate: {
-        type: Date,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    handledBy: {
-        type: String,
-        required: true
-    },
-    organisation: {
-        type: String,
-        required: true
-    },
-    totalSubEvents: {
-        type: Number,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
-    }
+  eventName: {
+    type: String,
+    required: true,
+  },
+  eventType: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  handledBy: {
+    type: String,
+    required: true,
+  },
+  organisation: {
+    type: String,
+    required: true,
+  },
+  totalSubEvents: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  platform: {
+    type: String,
+    enum: ['eventpro', 'eventease'],
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Event', EventSchema);
