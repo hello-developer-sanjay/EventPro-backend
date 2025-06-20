e44cfdf1d991be84791334865ad77c17442fabea
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 18:45:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,44 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/events');
-const authMiddleware = require('./middleware/authMiddleware');
-const cors = require('cors');  
-const passport = require('passport');
-    const session = require('express-session');
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const eventRoutes = require("./routes/events");
+const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 
-// Connect Database
 connectDB();
 
-// Init Middleware
+app.use(
+  cors({
+    origin: "https://event-ease-unified-event-manager.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "x-auth-token"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
+
 app.use(express.json({ extended: false }));
-  app.use(session({
-            secret: 'fRwD8ZcX#k5H*J!yN&2G@pQbS9v6E$tA', 
-            resave: false,
-            saveUninitialized: true,
-        }));
-// Enable CORS
-app.use(cors());  
- app.use(passport.initialize());
-        app.use(passport.session());
-        console.log('Passport middleware initialized');
-require('./config/passport'); // Include passport configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport");
 
-// Define Routes
-app.use('/api/auth', authRoutes); // Authentication routes
-app.use('/api/eventpro/events', eventRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
 
-// Define a basic route
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => {
+  console.log("Received request to root endpoint");
+  res.send("EventPro API Running");
+});
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
