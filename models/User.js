ebdca5321f94aa2a6ed6db1ea14172b728168787
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 14:01:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,31 +1,41 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-    },
-    role: {
-        type: String,
-        default: 'user',
-        enum: ['user', 'admin']
-    },
-    googleId: { type: String, unique: true, sparse: true },
-    resetPasswordToken: String, 
-    resetPasswordExpires: Date,
-    date: {
-        type: Date,
-        default: Date.now
-    },
-   
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['user', 'admin'],
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  platform: {
+    type: String,
+    enum: ['eventpro', 'eventease'],
+    default: 'eventpro',
+  },
+  googleAccessToken: String,
+  googleRefreshToken: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
